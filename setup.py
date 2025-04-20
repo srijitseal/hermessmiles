3b8e2589232de644b072a9e3797076e254376eaa
Srijit Seal <srijitseal@gmail.com>
2025-04-20 14:36:25 -0400

    update readme

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hermessmiles",
-    version="0.1.3",
+    version="0.1.4",
     author="Srijit Seal",
     author_email="srijit@understanding.bio",
     description="SMILES comparison tool with overlap visualization",
